b1374fb40b94dcd3f2ccf91f7f0e959c80d85adf
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-20 00:09:48 +0600

    Update KNN-Classifier2.py

--- KNN-Classifier2.py
+++ KNN-Classifier2.py
@@ -1,1 +1,11 @@
+#libraries
+
+import os
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
