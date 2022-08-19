23d90ea82d341e292ec2804effec8684618a875d
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-20 00:11:44 +0600

    Update KNN-Classifier2.py

--- KNN-Classifier2.py
+++ KNN-Classifier2.py
@@ -9,3 +9,18 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
+data= pd.read_csv('/content/Iris.csv')
+
+#feature selection
+
+feature_columns=['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm','PetalWidthCm']
+X= data[feature_columns].values
+y= data['Species'].values
+
+#spliting data
+
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size= 0.2, random_state= 0)
+
+#model create
+
+model= KNeighborsClassifier(n_neighbors=3)
